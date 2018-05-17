a8b8d3597ca82bfaed7a60a0ab0996b990d7d126
Dave Kasparov <dave.kasparov@gmail.com>
2018-05-17 21:50:45 +0000

    initial

---
+++ examples/example.js
@@ -0,0 +1,40 @@
+const PCF8574Cluster = require('../');
+const i2cBus = require('i2c-bus').openSync(1);
+
+const addresses = [0x20, 0x22];
+const initialStates = [true, true];
+
+const cluster = new PCF8574Cluster(i2cBus, addresses, initialStates);
+
+cluster.enableInterrupt(1, 4);
+cluster.enableInterrupt(2, 5)
+
+
+cluster.outputPin(1, true, false)
+.then(() => {
+	return cluster.outputPin(2, true, false);
+})
+.then(() => {
+	return cluster.inputPin(8, false);
+})
+.then(() => {
+	return cluster.inputPin(12, false);
+});
+
+
+cluster.on('input', (data) => {
+	console.log('input', data);
+
+	if (data.pin === 8) {
+		cluster.setPin(1, !cluster.getPinValue(1));
+	} else if(data.pin === 12){
+    cluster.setPin(1, !cluster.getPinValue(1));
+  }
+});
+
+
+process.on('SIGINT', function(){
+	console.log('SIGINT');
+  cluster.removeAllListeners();
+  cluster.disableInterrupt();
+});
---
+++ index.js
@@ -0,0 +1,119 @@
+const EventEmitter = require('events');
+const PCF8574 = require('pcf8574').PCF8574;
+
+const chai = require('chai');
+const chaiParam = require('chai-param');
+const expect = chai.expect;
+chai.use(chaiParam);
+
+class PCF8574Cluster extends EventEmitter {
+  constructor(i2cBus, addresses, initialStates) {
+		super();
+    //TODO: params validation
+		expect(addresses).param('addresses')
+		.to.be.an('array');
+
+		if (initialStates) {
+		
+		} else {
+			
+		}
+
+    this._pcf_instances = [];
+		this._expander_pins_count = 8;
+		this._expanders_count = addresses.length;
+		this._total_pins_count = this._expander_pins_count * this._expanders_count.length;
+
+    addresses.forEach((address, i) => {
+      let pcf = new PCF8574(i2cBus, address, initialStates[i]);
+
+			pcf.on('input', (data) => {
+				data.real_pin = data.pin;
+				data.pin = this._getPinByRealPin(i, data.real_pin);
+				data.expander_index = i;
+				this.emit('input', data);
+			});
+
+      this._pcf_instances.push(pcf);
+    });
+  }
+
+  enableInterrupt(index, pin) {
+		//TODO: params validationi
+
+		return this._pcf_instances[index - 1].enableInterrupt(pin);
+  }
+
+  disableInterrupt() {
+		this._pcf_instances.forEach(pcf => {
+      pcf.disableInterrupt();
+    });
+  }
+
+  inputPin(pin, inverted) {
+		//TODO:params validation
+
+		let index = this._getExpanderIndexByPin(pin);
+		let realPin = this._getRealPin(index, pin);	
+
+		return this._pcf_instances[index].inputPin(realPin, inverted);
+  }
+
+  outputPin(pin, inverted, initialValue) {
+		//TODO: params validation
+
+		let index = this._getExpanderIndexByPin(pin);
+		let realPin = this._getRealPin(index, pin);
+		
+		return this._pcf_instances[index].outputPin(realPin, inverted, initialValue);
+  }
+
+  setPin(pin, value) {
+		//TODO: params validation
+
+		let index = this._getExpanderIndexByPin(pin);
+		let realPin = this._getRealPin(index, pin);
+
+		return this._pcf_instances[index].setPin(realPin, value);
+  }
+
+  getPinValue(pin) {
+		//TODO: param validation
+
+		let index = this._getExpanderIndexByPin(pin);
+    let realPin = this._getRealPin(index, pin);
+
+		return this._pcf_instances[index].getPinValue(realPin);
+  }
+
+  setAllPins(value) {
+		this._pcf_instances.forEach(pcf => {
+			pcf.setAllPins(value);
+		});
+  }
+
+	removeAllListeners() {
+		this._pcf_instances.forEach(pcf => {
+      pcf.removeAllListeners();
+    });
+	}
+
+	_getExpanderIndexByPin(pin) {
+		return Math.ceil(pin / this._expander_pins_count) - 1;
+	}
+
+	_getRealPin(index, pin) {
+		return (pin - (index  * this._expander_pins_count)) - 1;
+	}
+
+	_getPinByRealPin(index, realPin) {
+		if (index == 0) {
+			return realPin + 1;
+		} else {
+			return (index * this._expander_pins_count) + realPin + 1;
+		}
+	}
+}
+
+module.exports = PCF8574Cluster;
+
